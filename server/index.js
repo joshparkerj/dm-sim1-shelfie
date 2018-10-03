025f982373693fda9df53e166b36dc6653f22d32
joshparkerj <superfakek@gmail.com>
2018-10-02 19:17:48 -0600

    require massive and dotenv in server index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const massive = require('massive');
+const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+massive(process.env.DB_URI)
+  .then(db => {
+    app.set('db',db);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 app.listen(4000, () => {
   console.log('now listening on 4000');
